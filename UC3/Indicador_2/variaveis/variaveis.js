67a20297671b61207dbb7f9c4212089c5702aa7b
Luciano <luciano@mundoslackware.com.br>
2024-05-28 17:06:51 -0300

    28-05

--- UC3/Indicador_2/variaveis/variaveis.js
+++ UC3/Indicador_2/variaveis/variaveis.js
@@ -1,7 +1,12 @@
 //declaração com VAR
 //console.log é o comando para imprimir na tela um resultado
-var nome = 'Jão'
+var nome = 'Jão de Caratinga'
 console.log(nome)
+console.log(nome.length) // Contar caracteres
+const n = nome.toUpperCase() // deixar caracteres em Maiusculo
+const n1 = nome.toLowerCase() // deixar caracteres em Minusculo
+console.log(n)
+console.log(n1)
 //declaração com LET
 //comando typeof é para identificar o tipo de dado da variavel
 let idade = true
@@ -9,7 +14,8 @@ console.log(typeof idade)
 //declaração com CONST
 const pi = 3.14
 //pi = 3.151515 erro ao tentar redeclarar a constante
-console.log(pi)
+
+
 
 
   
