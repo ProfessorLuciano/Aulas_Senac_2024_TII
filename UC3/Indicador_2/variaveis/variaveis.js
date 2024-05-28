725f6917da3467cae8b1763f78f32d96441242ee
Luciano <luciano@mundoslackware.com.br>
2024-05-28 16:54:15 -0300

    28-05

---
+++ UC3/Indicador_2/variaveis/variaveis.js
@@ -0,0 +1,16 @@
+//declaração com VAR
+//console.log é o comando para imprimir na tela um resultado
+var nome = 'Jão'
+console.log(nome)
+//declaração com LET
+//comando typeof é para identificar o tipo de dado da variavel
+let idade = true
+console.log(typeof idade)
+//declaração com CONST
+const pi = 3.14
+//pi = 3.151515 erro ao tentar redeclarar a constante
+console.log(pi)
+
+
+  
+
