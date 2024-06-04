8fdbe7f8d021832449771f7a231934b86815154c
Luciano <luciano@mundoslackware.com.br>
2024-06-04 17:17:10 -0300

    04-06

--- UC3/Indicador_2/Prompt/index.js
+++
@@ -1,8 +0,0 @@
-const prompt = require("prompt-sync")();
-
-let a = Number(prompt("Digite a primeira nota: "))
-let b = Number(prompt("Digite a segunda nota: "))
-let c = Number(prompt("Digite a terceira nota: "))
-let d = Number(prompt("Digite a quarta nota: "))
-let valor = (a + b + c + d) / 4
-console.log(valor)
