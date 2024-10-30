708779ac7e01cef4c74a5906d5ec46bd5f804984
Luciano Custodio <luciano.silva@sp.senac.br>
2024-10-30 13:35:02 -0300

    30-10

--- CRUD_LOGIN/frontend/src/Inicio/index.js
+++ CRUD_LOGIN/frontend/src/Inicio/index.js
@@ -1,9 +1,25 @@
 import React from 'react'
 
+import './estilo.inicio.scss'
+
 export default function Inicio(){
     return(
-        <div>
+        <div className='conteinerInicioGeral'>
         <h1>Pagina de Inicio</h1>
+
+        <form>
+            <input 
+            type="text"
+            placeholder='Digite o E-mail'
+            />
+
+            <input 
+            type="password"
+            placeholder='Digite a Senha'            
+            />
+
+            <button>Enviar</button>
+        </form>
         </div>
     )
 }
