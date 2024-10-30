d889d289e11ac582f40edda9e05bfa21b9889fdb
Luciano Custodio <luciano.silva@sp.senac.br>
2024-10-30 17:45:18 -0300

    30-10

--- CRUD_LOGIN/frontend/src/App.js
+++ CRUD_LOGIN/frontend/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react'
-
 import Rotas from './Routes'
+import { ToastContainer } from 'react-toastify'
 
 export default function App() {
   return (
     <div>
       <Rotas />
+      <ToastContainer
+        autoClose={5000}
+      />
     </div>
-      
+
   )
 }
 
--- CRUD_LOGIN/frontend/src/DashBoard/index.js
+++ CRUD_LOGIN/frontend/src/DashBoard/index.js
@@ -1,45 +1,63 @@
 import React, { useState, useEffect } from 'react'
 import './estilo.dashboard.scss'
-
+import { toast } from 'react-toastify'
 import apiLocal from './../Api/apiLocal'
+
 export default function DashBoard() {
 
     const [dadosUsuarios, setDadosUsuarios] = useState([''])
+
     useEffect(() => {
-        async function consultarDadosusuarios() {
-            const resposta = await apiLocal.get('/ConsultarUsuarios')
-            setDadosUsuarios(resposta.data)
+        try {
+            async function consultarDadosusuarios() {
+                const resposta = await apiLocal.get('/ConsultarUsuarios')
+                setDadosUsuarios(resposta.data)
+            }
+            consultarDadosusuarios()
+        } catch (err) {
+            toast.error('Erro ao Comunicar com BackEnd', {
+                toastId: 'ToastId'
+            })
         }
-        consultarDadosusuarios()
     }, [dadosUsuarios])
 
     return (
-        <div className='conteinerDashboardGeral'>
-            <h1>Pagina de DashBoard</h1>            
-                <table className='usuariosTabela'>
-                    <thead>
-                        <tr key="">
-                            <th>ID</th>
-                            <th>Nome</th>
-                            <th>Email</th>
-                            <th>Edita</th>
-                            <th>Apaga</th>
-                        </tr>
-                        {dadosUsuarios.map((item) => {
-                            return (
-                                <>
-                                    <tr key="">
-                                        <td>{item.id}</td>
-                                        <td>{item.nome}</td>
-                                        <td>{item.email}</td>
-                                        <td><button>Editar</button></td>
-                                        <td><button>Apagar</button></td>
-                                    </tr>
-                                </>
-                            )
-                        })}
-                    </thead>
-                </table>
-            </div>        
+        <>
+            {dadosUsuarios.length === 0
+                ?
+                <div className='conteinerDashboardGeral'>
+                    <h1>Pagina de DashBoard</h1>
+                    <h1>Sem dados</h1>
+                </div>
+                :
+                <div className='conteinerDashboardGeral'>
+                    <h1>Pagina de DashBoard</h1>
+                    <table className='usuariosTabela'>
+                        <thead>
+                            <tr key="">
+                                <th>ID</th>
+                                <th>Nome</th>
+                                <th>Email</th>
+                                <th>Edita</th>
+                                <th>Apaga</th>
+                            </tr>
+                            {dadosUsuarios.map((item) => {
+                                return (
+                                    <>
+                                        <tr key="">
+                                            <td>{item.id}</td>
+                                            <td>{item.nome}</td>
+                                            <td>{item.email}</td>
+                                            <td><button>Editar</button></td>
+                                            <td><button>Apagar</button></td>
+                                        </tr>
+                                    </>
+                                )
+                            })}
+                        </thead>
+                    </table>
+                </div>
+            }
+        </>
     )
 }
