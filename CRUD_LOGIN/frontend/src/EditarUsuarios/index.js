b4c95ce1bf13a328588d4052fd2521305f3bb873
Luciano Custodio <luciano.silva@sp.senac.br>
2024-11-12 14:38:35 -0300

    12-11

--- CRUD_LOGIN/frontend/src/EditarUsuarios/index.js
+++ CRUD_LOGIN/frontend/src/EditarUsuarios/index.js
@@ -1,12 +1,52 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
+import apiLocal from './../Api/apiLocal'
+import './estilo.editarUsuarios.scss'
 
-export default function EditarUsuarios(){
+export default function EditarUsuarios() {
     const { id } = useParams()
-    return(
-        <div>
-        <h1>Editar Usuários</h1>
-        {id}        
+    const [nome, setNome] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    useEffect(() => {
+        async function consultarDados(){
+            const resposta = await apiLocal.post('/ConsultarUsuariosUnico', {
+                id
+            })
+           setNome(resposta.data.nome)
+           setEmail(resposta.data.email)
+           setPassword(resposta.data.senha)
+        }
+        consultarDados()
+    }, [])
+
+    async function enviarAlteracao(e){
+        e.preventDefault()
+        alert(nome + email + password)
+    }
+
+    return (
+        <div className='conteinerEditarUsuariosGeral'>
+            <h1>Editar Usuários</h1>
+            <form onSubmit={enviarAlteracao}>
+                <input
+                    type="text"
+                    value={nome}
+                    onChange={(e) => setNome(e.target.value)}
+                />
+                <input
+                    type="text"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <input
+                    type="text"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <button type='submit'>Enviar</button>
+            </form>
         </div>
     )
 }
