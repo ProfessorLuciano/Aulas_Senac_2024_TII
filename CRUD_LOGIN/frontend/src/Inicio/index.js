e4c60acde369983c6c301f0647cbeba427455bbb
Luciano Custodio <luciano.silva@sp.senac.br>
2024-11-19 14:31:34 -0300

    19-11

--- CRUD_LOGIN/frontend/src/Inicio/index.js
+++ CRUD_LOGIN/frontend/src/Inicio/index.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 import './estilo.inicio.scss'
 
 
-export default function Inicio() { 
+export default function Inicio() {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
 
     return (
         <div className='conteinerInicioGeral'>
@@ -14,11 +17,15 @@ export default function Inicio() {
                 <input
                     type="text"
                     placeholder='Digite o E-mail'
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
                 />
 
                 <input
                     type="password"
                     placeholder='Digite a Senha'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
                 />
 
                 <button>Enviar</button>
